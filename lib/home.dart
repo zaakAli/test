75ab2466cb79bc0157821e2a19d3e5d3e2835ece
zaakAli <zaak4845@gmail.com>
2025-07-20 15:26:38 +0300

    added color and format to home screen

--- lib/home.dart
+++ lib/home.dart
@@ -7,16 +7,29 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
         child: Column(
           children: [
-            SizedBox(height: 50,),
-            Center(child: Text('this is home page', style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),),
-               SizedBox(height: 23,),
-            Center(child: Text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley'),),
-          
+            SizedBox(height: 50),
+            Center(
+              child: Text(
+                'This is home page',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+            SizedBox(height: 23),
+            Center(
+              child: Text(
+                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley',
+              ),
+            ),
           ],
         ),
-      ));
+      ),
+    );
   }
 }
