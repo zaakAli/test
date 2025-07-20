df061d04ebc7f61380100a5d963e26e863234f37
zaakAli <zaak4845@gmail.com>
2025-07-20 15:19:27 +0300

    added container ,sizebox ,text center to home screen

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,18 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('this is home page'),));
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 50,),
+            Center(child: Text('this is home page', style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),),
+               SizedBox(height: 23,),
+            Center(child: Text('Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley'),),
+          
+          ],
+        ),
+      ));
   }
 }
